c50d1def41089e621d832bba7731a96b5a8158c9
overmind <seogarry@gmail.com>
2017-05-18 23:47:24 +0300

    Implement statuses api endpoint for gathering data from servers

---
+++ app/Http/Controllers/StatusController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Status;
+use App\Server;
+use Illuminate\Http\Request;
+
+class StatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        //echo 1;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        echo 2;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->json()->all();
+        if ($data) {
+            $status = new Status;
+            $status->status = $data['status'];
+            $status->server_id = $data['serverId'];
+            $status->data = json_encode($data);
+
+            if ($status->save()) {
+                return response()->json($status);
+            }
+        }
+        return response()->json([], 500);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Status  $status
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Status $status)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Status  $status
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Status $status)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Status  $status
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Status $status)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Status  $status
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Status $status)
+    {
+        //
+    }
+}
